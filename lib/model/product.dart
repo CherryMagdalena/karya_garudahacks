76fc324d885ccaf7b812c6d1efe9e924a09592ea
Cherry <cherry_magdalena@yahoo.co.id>
2020-08-15 00:10:35 +0800

    Created Product class

---
+++ lib/model/product.dart
@@ -0,0 +1,12 @@
+class Product {
+  final String username, category, image, title, description;
+  final int price;
+
+  Product(
+      {this.username,
+      this.category,
+      this.image,
+      this.title,
+      this.description,
+      this.price});
+}
