c8b886f4035e5642ee23989536d4812984a8382d
Samantha <samsciencestream@gmail.com>
2020-08-15 02:08:18 +0800

    color and theme added, until order success page

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,52 +1,82 @@
 // Settings Screen, profile etc
 
 import 'package:flutter/material.dart';
-
-GestureDetector _container(String label, {Function onClicked}) {
-  return GestureDetector(
-    onTap: onClicked,
-    child: Container(
-      width: 200.0,
-      padding: EdgeInsets.all(5.0),
-      decoration: BoxDecoration(
-
-      ),
-      child: Center(
-        child: Text(
-          label,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 18,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    ),
-  );
-}
+import 'package:karya_garudahacks/model/colors.dart';
+import 'package:karya_garudahacks/components/app_bar.dart';
+import 'package:karya_garudahacks/components/bottom_app_bar.dart';
 
 class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:<Widget> [
-        Row(
-          children: [
-            Icon(Icons.people,
-            size:40,
-            ),
-            _container('Profile'),
-            _container('My Post'),
-            _container('Purchases'),
-            _container('My Likes'),
-            _container('Settings'),
-            _container('Log Out'),
-          ],
 
 
+    Column settings() {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 15.0),
+                  child: Icon(
+                    Icons.people,
+                    size: 40,
+                  ),
+                ),
+                    Text(
+                      'INSERT USERNAME',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.black87,
+                      ),
+                    ),
+
+
+              ]),
+          _container('Profile', onClicked: () {
+           //to profile screen
+          }),
+          _container('My Post'),
+          _container('Purchases'),
+          _container('My Likes'),
+          _container('Setting'),
+          _container('Log Out', onClicked: (){
+            //log out
+    }),
+        ],
+      );
+    }
+
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: color4,
+            bottomNavigationBar: CustomBottomAppBar(),
+            appBar: CustomAppBar(),
+            body: settings()));
+  }
+
+  GestureDetector _container(String label, {Function onClicked}) {
+    return GestureDetector(
+      onTap: onClicked,
+      child: Container(
+        width: 200.0,
+        padding: EdgeInsets.all(5.0),
+        decoration: BoxDecoration(
+          color: color2,
         ),
-      ],
+        child: Center(
+          child: Text(
+            label,
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
