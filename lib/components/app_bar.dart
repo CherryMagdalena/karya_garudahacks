b70c197bc3258cc8659853703fe6251faa826455
Cherry <cherry_magdalena@yahoo.co.id>
2020-08-14 21:50:58 +0800

    Created Custom AppBar

---
+++ lib/components/app_bar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:karya_garudahacks/model/colors.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double height;
+  CustomAppBar({this.height = kToolbarHeight});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      iconTheme: IconThemeData(color: color1),
+      backgroundColor: color3,
+      elevation: 0,
+      automaticallyImplyLeading: true,
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 0.5),
+          child: IconButton(
+            icon: Icon(Icons.shopping_basket),
+            color: color1,
+            onPressed: (){
+              //To shopping cart page
+            },
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right:25.0),
+          child: IconButton(
+            icon:Icon(Icons.chat),
+            onPressed: (){
+              //To chatting page
+            },
+          )
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+}
