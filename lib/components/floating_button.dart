0854fbad04fb4f3222a899652ee8c6263535edbd
hendycopratama <hendycopratama777@gmail.com>
2020-08-15 08:11:27 +0700

    floating button (to navigate to upload page) added

---
+++ lib/components/floating_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:karya_garudahacks/model/colors.dart';
+
+class FloatingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(
+        Icons.add_circle_outline,
+        color: Colors.white,
+      ),
+      backgroundColor: color2,
+      //Navigate to upload screen
+      //onPressed: () => ,
+    );
+  }
+}
