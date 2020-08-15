1d18c7ff20fcdd581d302bd931aefb36421ed002
Cherry <cherry_magdalena@yahoo.co.id>
2020-08-15 22:02:23 +0800

    Fixed shopping_basket delete button

--- lib/screens/shopping_basket.dart
+++ lib/screens/shopping_basket.dart
@@ -7,23 +7,22 @@ import 'package:karya_garudahacks/model/product.dart';
 import 'package:karya_garudahacks/screens/checkout_multiple.dart';
 import 'package:karya_garudahacks/components/price_formatter.dart';
 
-
 class ShoppingBasket extends StatefulWidget {
   @override
   _ShoppingBasketState createState() => _ShoppingBasketState();
 }
 
 class _ShoppingBasketState extends State<ShoppingBasket> {
-  clearCart(){
+  clearCart() {
     ShoppingCart.tocart.clear();
   }
-  clearQTY(){
+
+  clearQTY() {
     ShoppingCart.qty.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-
     Product products;
     return Scaffold(
       backgroundColor: color4,
@@ -32,137 +31,116 @@ class _ShoppingBasketState extends State<ShoppingBasket> {
           backgroundColor: color3,
           elevation: 0,
           automaticallyImplyLeading: true,
-          title: Text('Cart',
+          title: Text(
+            'Cart',
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
-            ),)
-      ),
+            ),
+          )),
       body: ListView.builder(
         itemCount: ShoppingCart.tocart.length,
         itemBuilder: (context, index) {
-
-          return
-            Card(
-              child: ListTile(
-                leading: Image.network(
-                  ShoppingCart.tocart[index].image,
-                  width: 100.0,
-                  height: 100.0,
-                ),
-                title: Text(ShoppingCart.tocart[index].title),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-
-
-
-                    Text('Price(1) :'),
-
-
-                          Text(
-                              priceFormatter(
-                                  ShoppingCart.tocart[index].price),
-                              )
-
-
-
-
-                  ],
-                ),
-                trailing: new Wrap(
-                  direction: Axis.vertical,
-                  children: <Widget>[
-                    new IconButton(
-                      icon: Icon(Icons.arrow_drop_up),
-                      onPressed: (){
-                        setState(() {
-                          ShoppingCart.qty[index] += 1;
-                        });
-                      },
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 15.0),
-                      child: Text(
-                        ShoppingCart.qty[index].toString(),
-                      ),
-                    ),
-                    new IconButton(
-                      icon: Icon(Icons.arrow_drop_down),
-                      onPressed: () {
-                        setState(() {
-                          if (ShoppingCart.qty[index] == 1) {
-                            //ShoppingCart.qty[index];
-                          }
-                          else {
-                            ShoppingCart.qty[index] -= 1;
-                          }
-
-                        });
-                      },
-                    ),
-                    new IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: (){
-                        setState(() {
-                          ShoppingCart.tocart.remove(products);
-                        });
-                      },
-                    ),
-
-                  ],
-                ),
+          return Card(
+            child: ListTile(
+              leading: Image.network(
+                ShoppingCart.tocart[index].image,
+                width: 100.0,
+                height: 100.0,
               ),
-            );
-
+              title: Text(ShoppingCart.tocart[index].title),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Price(1) :'),
+                  Text(
+                    priceFormatter(ShoppingCart.tocart[index].price),
+                  )
+                ],
+              ),
+              trailing: new Wrap(
+                direction: Axis.vertical,
+                children: <Widget>[
+                  new IconButton(
+                    icon: Icon(Icons.arrow_drop_up),
+                    onPressed: () {
+                      setState(() {
+                        ShoppingCart.qty[index] += 1;
+                      });
+                    },
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 15.0),
+                    child: Text(
+                      ShoppingCart.qty[index].toString(),
+                    ),
+                  ),
+                  new IconButton(
+                    icon: Icon(Icons.arrow_drop_down),
+                    onPressed: () {
+                      setState(() {
+                        if (ShoppingCart.qty[index] == 1) {
+                          //ShoppingCart.qty[index];
+                        } else {
+                          ShoppingCart.qty[index] -= 1;
+                        }
+                      });
+                    },
+                  ),
+                  new IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      setState(() {
+                        ShoppingCart.tocart.remove(ShoppingCart.tocart[index]);
+                        ShoppingCart.qty.removeAt(index);
+                        print(ShoppingCart.qty.length);
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
         },
       ),
-
       bottomNavigationBar: Container(
         color: color3,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-
-              MaterialButton(
-                onPressed:(){
-                  setState(() {
-                    clearCart();
-                    clearQTY();
-                  });
-                },
-                child: Text('Clear Cart'),
-              ),
-
+            MaterialButton(
+              onPressed: () {
+                setState(() {
+                  clearCart();
+                  clearQTY();
+                });
+              },
+              child: Text('Clear Cart'),
+            ),
             Expanded(
               child: ListTile(
                 title: Text('Total: '),
                 subtitle: Text(
-                  priceFormatter(
-    ShoppingCart.countTotalPrice()
-    ),
-
+                  priceFormatter(ShoppingCart.countTotalPrice()),
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
             ),
             MaterialButton(
-              onPressed: (){
-                Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) => CheckoutCart()
-                  ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CheckoutCart()),
                 );
               },
-              child: Text('Checkout',
+              child: Text(
+                'Checkout',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
           ],
         ),
       ),
-
-
     );
   }
 }
