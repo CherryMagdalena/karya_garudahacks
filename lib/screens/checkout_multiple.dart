08770666f256c285a855d76d4c44696c78f08c41
Cherry <cherry_magdalena@yahoo.co.id>
2020-08-16 03:48:57 +0800

    Edited UI (3)

--- lib/screens/checkout_multiple.dart
+++ lib/screens/checkout_multiple.dart
@@ -1,5 +1,6 @@
 // Checkout for cart
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:karya_garudahacks/model/colors.dart';
 import 'package:karya_garudahacks/screens/order_success_screen.dart';
@@ -7,31 +8,49 @@ import 'package:karya_garudahacks/model/shopping_cart.dart';
 import 'package:karya_garudahacks/components/price_formatter.dart';
 
 class CheckoutCart extends StatelessWidget {
-  clearCart(){
+  clearCart() {
     ShoppingCart.tocart.clear();
   }
+
   clearQTY() {
     ShoppingCart.qty.clear();
   }
+
   @override
   Widget build(BuildContext context) {
     Widget personaldetails = Container(
-      padding:
-      const EdgeInsets.only(left: 20.0, top: 20.0),
+      height: 250.0,
+      padding: const EdgeInsets.only(left: 20.0, top: 20.0),
       alignment: Alignment.centerLeft,
-      child:Column(
-
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children:[
-          Text('Personal Details',style: TextStyle( color: color1, fontSize: 20.0,fontWeight: FontWeight.bold), ),
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'Personal Details',
+            style: TextStyle(
+                color: color1, fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
           Text(''),
-          Text('Name: ', style: TextStyle( color: color1, fontSize: 20.0),),
+          Text(
+            'Name: ',
+            style: TextStyle(color: color1, fontSize: 18.0),
+          ),
           Text('Mario Luigi'),
-          Text('Address: ', style: TextStyle( color: color1, fontSize: 20.0),),
+          Text(
+            'Address: ',
+            style: TextStyle(color: color1, fontSize: 18.0),
+          ),
           Text('Mushroom Kingdom, Brooklyn, New York'),
-          Text('Contact Number: ', style: TextStyle( color: color1, fontSize: 20.0),),
+          Text(
+            'Contact Number: ',
+            style: TextStyle(color: color1, fontSize: 18.0),
+          ),
           Text('+62 111 1111 1111'),
-          Text('Payment Method: ', style: TextStyle( color: color1, fontSize: 20.0),),
+          Text(
+            'Payment Method: ',
+            style: TextStyle(color: color1, fontSize: 18.0),
+          ),
           Text('BCA Transfer'),
         ],
       ),
@@ -39,33 +58,35 @@ class CheckoutCart extends StatelessWidget {
 
     //delivery
     Widget deliveryoptions = Container(
-      padding:
-      const EdgeInsets.only(left: 20.0, bottom: 20.0, top: 10.0),
+      padding: const EdgeInsets.only(left: 20.0, bottom: 20.0, top: 5.0),
       alignment: Alignment.centerLeft,
-      child:Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Delivery Method', style: TextStyle( color: color1, fontSize: 20.0,fontWeight: FontWeight.bold),),
-          Text(''),
-          Text('JNE'),
+          Text(
+            'Delivery Method',
+            style: TextStyle(
+                color: color1, fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          Padding(padding: EdgeInsets.only(top: 5.0), child: Text('JNE')),
         ],
       ),
     );
 
     return Scaffold(
       backgroundColor: color4,
-      appBar:  AppBar(
+      appBar: AppBar(
           iconTheme: IconThemeData(color: color1),
           backgroundColor: color3,
           elevation: 0,
           automaticallyImplyLeading: true,
-          title: Text('Checkout',
+          title: Text(
+            'Checkout',
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
-            ),)
-      ),
-
+            ),
+          )),
       body: Container(
         child: Column(
           children: [
@@ -74,58 +95,46 @@ class CheckoutCart extends StatelessWidget {
                 child: ListView.builder(
                   itemCount: ShoppingCart.tocart.length,
                   itemBuilder: (context, index) {
-
-                    return
-                      Card(
-                        child: ListTile(
-                          leading: Image.network(
-                            ShoppingCart.tocart[index].image,
-                            width: 100.0,
-                            height: 100.0,
-                          ),
-                          title: Text(
-                             ShoppingCart.tocart[index].title
-                          ),
-                          subtitle: Column(
-                            children: <Widget>[
-                              Row(
-                                children: <Widget>[
-                                  Expanded(
-                                    child: Text('Price(1) :'),
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                      priceFormatter(
-                                          ShoppingCart.tocart[index].price
-                                      )
-
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          trailing: new Wrap(
-                            direction: Axis.vertical,
-                            children: <Widget>[
-                              Container(
-                                padding: EdgeInsets.only(top: 15.0),
-                                child: Text( 'Quantity: '
-
-                                ),
-                              ),
-                              Container(
-                                padding: EdgeInsets.only(top: 15.0),
-                                child: Text(
-                                  ShoppingCart.qty[index].toString(),
-                                ),
-                              ),
-
-                            ],
-                          ),
+                    return Card(
+                      child: ListTile(
+                        leading: Image.network(
+                          ShoppingCart.tocart[index].image,
+                          width: 100.0,
+                          height: 100.0,
                         ),
-                      );
-
+                        title: Text(ShoppingCart.tocart[index].title),
+                        subtitle: Column(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Text('Price(1) :'),
+                                ),
+                                Expanded(
+                                  child: Text(priceFormatter(
+                                      ShoppingCart.tocart[index].price)),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        trailing: new Wrap(
+                          direction: Axis.vertical,
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.only(top: 15.0),
+                              child: Text('Quantity: '),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(top: 15.0),
+                              child: Text(
+                                ShoppingCart.qty[index].toString(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
                   },
                 ),
               ),
@@ -135,7 +144,6 @@ class CheckoutCart extends StatelessWidget {
           ],
         ),
       ),
-
       bottomNavigationBar: Container(
         color: color3,
         child: Row(
@@ -143,34 +151,30 @@ class CheckoutCart extends StatelessWidget {
           children: <Widget>[
             Expanded(
               child: ListTile(
-                title:Text('Total: '),
+                title: Text('Total: '),
                 subtitle: Text(
-                  priceFormatter(
-                    ShoppingCart.countTotalPrice()
-                  ),
-
+                  priceFormatter(ShoppingCart.countTotalPrice()),
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
             ),
             MaterialButton(
-              onPressed: (){
+              onPressed: () {
                 clearCart();
                 clearQTY();
-                Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) => OrderSuccess()
-                  ),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => OrderSuccess()),
                 );
               },
-              child: Text('Make Order',
+              child: Text(
+                'Make Order',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ),
           ],
         ),
       ),
-
     );
   }
 }
